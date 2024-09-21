c6ee58c60338a59ccf9a29363425e245cbe9d66c
Argie <argiedc888@gmail.com>
2024-09-21 09:57:22 +0800

    practice input output

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!!!");
+        Console.WriteLine("This is how to print in C#");
+
+        Console.Write("What is the color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"The color is {color}");
     }
 }
