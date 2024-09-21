d0f84c9d4b29b0a171b5a4841f102ac1c95599a0
Argie <argiedc888@gmail.com>
2024-09-21 21:50:01 +0800

    practice loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int intmagic = randomGenerator.Next(1, 101);
+        
+        int intguess = -1;
+        while (intmagic != intguess)
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            intguess = int.Parse(guess);
+            if (intguess > intmagic)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (intguess < intmagic)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (intguess == intmagic)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+
+        }
+
+
     }
 }
